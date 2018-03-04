6d692f78997cdec2dd7be9d97f18a1b9fded01d6
Rufin Hounkpe <rhounkpe@yahoo.fr>
2018-03-04 16:05:51 +0100

    Template engine configured

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 const express = require('express');
+const keys = require('./config/dev');
+const stripe = require('stripe')(keys.stripe.publishableKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+
+const app = express();
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+// Set Static Folder
+app.use(express.static(`${__dirname}/public`));
+
+// Index Route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
+
